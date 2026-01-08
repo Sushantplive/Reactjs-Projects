068adedc6f34f178f77daa29b7c280ef5d7efea3
Sushant Paikrao <35286203+Sushantplive@users.noreply.github.com>
2026-01-08 15:53:16 +0000

    handleUseEffect

--- react-hooks/src/hooks/Component/VideoPlayer.jsx
+++ react-hooks/src/hooks/Component/VideoPlayer.jsx
@@ -9,8 +9,9 @@ const VideoPlayer = ({src, isPlaying}) => {
     //     ref.current.pause() // this is also break
     // )
     /**
-     * In above code we are try to modify DOM while it is rendering.
-     * we are taking ref of video tag but react doesnt not render it 
+     * In above code we are try to modify DOM while it is rendering. it is a side effect
+     * we are taking ref of video tag but react doesnt not render it.
+     * unless react does not return jsx so react does not know which dom need to create.
      * so we cant find its refrence, there fore it break.
      * to fix this we need to wrap this in useEffect.
      * in this case react first render the <video> tag in DOM then it will run the effect 
@@ -19,11 +20,16 @@ const VideoPlayer = ({src, isPlaying}) => {
      */
     useEffect(() => {
         if (isPlaying) {
+            console.log('calling play method')
             ref.current.play() // VideoPlayer.jsx?t=1767879026680:11 Uncaught TypeError: Cannot read properties of null (reading 'pause')
-        } else (
+        } else {
+            console.log('calling pause method')
             ref.current.pause() // this is also break
-        )
-    },[isPlaying])
+        }
+    },[isPlaying]) 
+    // so in app component input state got change but because  isPlaying state not chnage so this effect will not run,
+    // other wise it will run every state change
+
 
     return (
         <div>
--- react-hooks/src/hooks/UseEffectComponent.jsx
+++ react-hooks/src/hooks/UseEffectComponent.jsx
@@ -15,12 +15,18 @@ const UseEffectComponent = () => {
     // },[])
 
     const [isPlaying, setIsPlaying] = useState(false)
+    const [text, setText] = useState('')
 
     const handleClickForVideoPlay =() =>{
         setIsPlaying(true)
     }
     const handleClickForVideoPause =() =>{
         setIsPlaying(false)
+    }
+
+    // text handler
+    const handleInputChage = (e) =>{
+        setText(e.target.value)
     }
 
 
@@ -31,8 +37,11 @@ const UseEffectComponent = () => {
             {/* <button onClick={() => setIsPlaying(!isPlaying)}>
                 {isPlaying ? 'Pause' : 'Play'}
             </button> */}
-            <button onClick={handleClickForVideoPlay} style={{backgroundColor:'lightblue'}}>Play</button><br/>
-            <button onClick={handleClickForVideoPause} style={{backgroundColor:'lightpink'}}>Pause</button>
+            <div>
+            <input className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" type='text' value={text} onChange={handleInputChage}/>
+            </div>
+            <button className="text-white bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none" onClick={handleClickForVideoPlay} style={{backgroundColor:'lightblue'}}>Play</button><br/>
+            <button className="text-white bg-danger box-border border border-transparent hover:bg-danger-strong focus:ring-4 focus:ring-danger-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none" onClick={handleClickForVideoPause} style={{backgroundColor:'lightpink'}}>Pause</button>
             <VideoPlayer src={"/flower.webm"} isPlaying={isPlaying} />
         </div>
     )
