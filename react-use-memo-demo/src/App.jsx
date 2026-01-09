eccb794887031cfaf632e0f09c194cc96a303af4
Sushant Paikrao <35286203+Sushantplive@users.noreply.github.com>
2026-01-09 10:58:55 +0000

    useMemo demo

---
+++ react-use-memo-demo/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  console.log('welcome to useMemo')
+  return (
+    <>
+      <h2>Welcome to react use memo demo</h2>
+      <hr/>
+    </>
+  )
+}
+
+export default App
